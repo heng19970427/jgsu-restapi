8225c771325b1e09de7be8959f95871f1fadc8a9
ubuntu <ubuntu@localhost.localdomain>
2018-08-05 15:26:07 +0800

    zhouci.py

--- zhouci.py
+++ zhouci.py
@@ -5,16 +5,7 @@ from db import Dao
 dao = Dao()
 stu = Student(Account=1609103050, PWD='xiaoliu...')
 
-<<<<<<< HEAD
 stu.login()
 classes,xq,zc = stu.getKeBiao()
 dao.update_xqzc(xq,zc)
 print(xq,zc)
-=======
-while True:
-    stu.login()
-    classes,xq,zc = stu.getKeBiao()
-    dao.update_xqzc(xq,zc)
-    time.sleep(86400)
-    
->>>>>>> 496e281f4f061eb6afc6d3b1fb14cea35518dd4b
